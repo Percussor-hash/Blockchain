eed951b74c8bd76504fc6056f0df5a370c00d489
Percussor-hash <85029612+Percussor-hash@users.noreply.github.com>
2022-07-21 23:58:38 +0530

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+contract SimpleStorage {
+    uint256 favoriteNumber;
+    bool favoriteBool;
+
+    struct People{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+    mapping(string => uint256) public nameToFavNumber;
+
+    function store(uint256 _favnum) public {
+        favoriteNumber = _favnum;
+    }  
+
+    function retrieve()public view returns(uint256){
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public{
+        people.push(People(_favoriteNumber, _name));
+        nameToFavNumber[_name] = _favoriteNumber;
+    }
+
+
+
+}
